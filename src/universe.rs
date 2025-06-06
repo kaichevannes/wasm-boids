e16c69d7767c5dc69cbb4f096014e60cae53aa0c
Kai Chevannes <chevannes.kai@gmail.com>
2025-06-06 19:00:27 +0100

    test: generated boids should be unique

--- src/universe.rs
+++ src/universe.rs
@@ -100,3 +100,95 @@ impl Universe {
         &self.boids
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::{boid::Boid, *};
+    use std::collections::VecDeque;
+
+    #[test]
+    fn builds_with_expected_number_of_boids() {
+        let universe_with_no_boids = universe::Builder::from_preset(universe::Preset::Basic)
+            .boid_count(0)
+            .build();
+        assert!(universe_with_no_boids.get_boids().is_empty());
+
+        let universe_with_single_boid = universe::Builder::from_preset(universe::Preset::Basic)
+            .boid_count(1)
+            .build();
+        assert_eq!(1, universe_with_single_boid.get_boids().len());
+
+        let universe_with_one_hundred_boids =
+            universe::Builder::from_preset(universe::Preset::Basic)
+                .boid_count(100)
+                .build();
+        assert_eq!(100, universe_with_one_hundred_boids.get_boids().len());
+    }
+
+    #[test]
+    fn builds_with_expected_boid_values() {
+        let boids: Vec<Boid> = (0..3)
+            .map(|n| {
+                let x = n as f32;
+                Boid {
+                    position: (x, x),
+                    velocity: (x, x),
+                    acceleration: (x, x),
+                }
+            })
+            .collect();
+        let universe = universe::Builder::from_preset(universe::Preset::Basic)
+            .boid_count(boids.len() as u32)
+            .create_boid({
+                let mut boids_copy: VecDeque<Boid> = boids.clone().into();
+                move || boids_copy.pop_front().unwrap()
+            })
+            .build();
+        assert!(
+            universe
+                .get_boids()
+                .iter()
+                .zip(boids.iter())
+                .all(|(expected, actual)| expected.position == actual.position)
+        )
+    }
+
+    #[test]
+    fn tick_changes_boid_positions() {
+        let mut universe = Universe::build_from_preset(universe::Preset::Basic);
+        let original_positions: Vec<(f32, f32)> = universe
+            .get_boids()
+            .iter()
+            .map(|boid| boid.position)
+            .collect();
+        universe.tick();
+        let updated_positions: Vec<(f32, f32)> = universe
+            .get_boids()
+            .iter()
+            .map(|boid| boid.position)
+            .collect();
+        assert!(
+            original_positions
+                .iter()
+                .zip(updated_positions.iter())
+                .all(|(before, after)| before != after)
+        );
+    }
+
+    #[test]
+    fn builds_with_unique_boid_states() {
+        let universe = Universe::build_from_preset(universe::Preset::Basic);
+        let mut seen_positions = vec![];
+        let mut seen_velocities = vec![];
+        let mut seen_accelerations = vec![];
+        for boid in universe.get_boids() {
+            assert!(!seen_positions.contains(&boid.position));
+            assert!(!seen_velocities.contains(&boid.velocity));
+            assert!(!seen_accelerations.contains(&boid.acceleration));
+
+            seen_positions.push(boid.position);
+            seen_velocities.push(boid.velocity);
+            seen_accelerations.push(boid.acceleration);
+        }
+    }
+}
