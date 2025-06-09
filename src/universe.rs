88ac0cd2e898bfde010c5763eac23e900b3fbd98
Kai Chevannes <chevannes.kai@gmail.com>
2025-06-09 18:28:30 +0100

    feat: transfer preset from temp to universe builder

--- src/universe.rs
+++ src/universe.rs
@@ -19,6 +19,18 @@ impl BlueNoiseBoidFactory {
             noise: BlueNoise::new(),
         }
     }
+
+    fn generate_samples_from_existing(
+        &mut self,
+        grid: &dyn Grid<Boid>,
+        existing_samples: Vec<Sample>,
+        number_of_samples_to_generate: u32,
+    ) -> Vec<Sample> {
+        let mut new_samples_grid = NaiveGrid::new(grid.get_size());
+        new_samples_grid.set_points(&existing_samples.iter().collect::<Vec<&Sample>>());
+        self.noise
+            .generate(&new_samples_grid, number_of_samples_to_generate)
+    }
 }
 
 impl BoidFactory for BlueNoiseBoidFactory {
@@ -33,20 +45,14 @@ impl BoidFactory for BlueNoiseBoidFactory {
             existing_accelerations.push(Sample(b.acceleration.0, b.acceleration.1));
         });
 
-        let mut new_position_grid = NaiveGrid::new(grid.get_size());
-        new_position_grid.set_points(&existing_positions.iter().collect::<Vec<&Sample>>());
-        let mut new_velocities_grid = NaiveGrid::new(grid.get_size());
-        new_velocities_grid.set_points(&existing_velocities.iter().collect::<Vec<&Sample>>());
-        let mut new_accelerations_grid = NaiveGrid::new(grid.get_size());
-        new_accelerations_grid.set_points(&existing_accelerations.iter().collect::<Vec<&Sample>>());
-
-        let new_positions = self.noise.generate(&new_position_grid, number_of_boids);
-        let new_velocities = self.noise.generate(&new_velocities_grid, number_of_boids);
-        let new_accelerations = self
-            .noise
-            .generate(&new_accelerations_grid, number_of_boids);
-
         let mut result = Vec::new();
+        let new_positions =
+            self.generate_samples_from_existing(grid, existing_positions, number_of_boids);
+        let new_velocities =
+            self.generate_samples_from_existing(grid, existing_velocities, number_of_boids);
+        let new_accelerations =
+            self.generate_samples_from_existing(grid, existing_accelerations, number_of_boids);
+
         for i in 0..number_of_boids {
             result.push(Boid {
                 position: new_positions.get(i as usize).unwrap().xy(),
@@ -69,6 +75,14 @@ pub enum Preset {
 #[wasm_bindgen]
 pub struct Builder {
     number_of_boids: Option<u32>,
+    density: Option<f32>,
+    noise_fraction: Option<f32>,
+    attraction_weighting: Option<u32>,
+    alignment_weighting: Option<u32>,
+    separation_weighting: Option<u32>,
+    attraction_radius: Option<f32>,
+    alignment_radius: Option<f32>,
+    separation_radius: Option<f32>,
     boid_factory: Box<dyn BoidFactory>,
 }
 
@@ -76,14 +90,84 @@ pub struct Builder {
 impl Builder {
     pub fn from_preset(preset: Preset) -> Builder {
         match preset {
-            Preset::Basic => Builder::default().number_of_boids(100),
-            Preset::Maruyama => Builder::default().number_of_boids(100),
-            Preset::Zhang => Builder::default().number_of_boids(100),
+            Preset::Basic => Builder::default()
+                .number_of_boids(100)
+                .density(1.0)
+                .noise_fraction(0.05)
+                .attraction_weighting(1)
+                .alignment_weighting(1)
+                .separation_weighting(1)
+                .attraction_radius(1.0)
+                .alignment_radius(1.0)
+                .separation_radius(1.0),
+            Preset::Maruyama => Builder::default()
+                .number_of_boids(100)
+                .density(600.0)
+                .noise_fraction(0.0)
+                .attraction_weighting(4)
+                .alignment_weighting(30)
+                .separation_weighting(1)
+                .attraction_radius(0.05)
+                .alignment_radius(0.05)
+                .separation_radius(0.01),
+            Preset::Zhang => Builder::default()
+                .number_of_boids(100)
+                .density(100.0)
+                .noise_fraction(0.05)
+                .attraction_weighting(10)
+                .alignment_weighting(1)
+                .separation_weighting(400)
+                .attraction_radius(0.05)
+                .alignment_radius(0.05)
+                .separation_radius(0.05),
         }
     }
 
     pub fn number_of_boids(mut self, count: u32) -> Self {
         self.number_of_boids = Some(count);
+        self
+    }
+
+    pub fn density(mut self, density: f32) -> Self {
+        self.density = Some(density);
+        self
+    }
+
+    pub fn noise_fraction(mut self, noise_fraction: f32) -> Self {
+        if !(0.0..=1.0).contains(&noise_fraction) {
+            panic!("Noise fraction must be between 0 and 1");
+        }
+        self.noise_fraction = Some(noise_fraction);
+        self
+    }
+
+    pub fn attraction_weighting(mut self, weighting: u32) -> Self {
+        self.attraction_weighting = Some(weighting);
+        self
+    }
+
+    pub fn alignment_weighting(mut self, weighting: u32) -> Self {
+        self.alignment_weighting = Some(weighting);
+        self
+    }
+
+    pub fn separation_weighting(mut self, weighting: u32) -> Self {
+        self.separation_weighting = Some(weighting);
+        self
+    }
+
+    pub fn attraction_radius(mut self, radius: f32) -> Self {
+        self.attraction_radius = Some(radius);
+        self
+    }
+
+    pub fn alignment_radius(mut self, radius: f32) -> Self {
+        self.alignment_radius = Some(radius);
+        self
+    }
+
+    pub fn separation_radius(mut self, radius: f32) -> Self {
+        self.separation_radius = Some(radius);
         self
     }
 
@@ -94,6 +178,26 @@ impl Builder {
         Universe {
             boids: (self.boid_factory).create_n(&NaiveGrid::<Boid>::new(100.0), number_of_boids),
             boid_factory: self.boid_factory,
+            density: self.density.expect("Must provide density"),
+            noise_fraction: self.noise_fraction.expect("Must provide noise_fraction"),
+            attraction_weighting: self
+                .attraction_weighting
+                .expect("Must provide attraction_weighting"),
+            alignment_weighting: self
+                .alignment_weighting
+                .expect("Must provide alignment_weighting"),
+            separation_weighting: self
+                .separation_weighting
+                .expect("Must provide separation_weighting"),
+            attraction_radius: self
+                .attraction_radius
+                .expect("Must provide attraction_radius"),
+            alignment_radius: self
+                .alignment_radius
+                .expect("Must provide alignment_radius"),
+            separation_radius: self
+                .separation_radius
+                .expect("Must provide separation_radius"),
         }
     }
 }
@@ -110,6 +214,14 @@ impl Default for Builder {
         Builder {
             number_of_boids: None,
             boid_factory: Box::new(BlueNoiseBoidFactory::new()),
+            density: None,
+            noise_fraction: None,
+            attraction_weighting: None,
+            alignment_weighting: None,
+            separation_weighting: None,
+            attraction_radius: None,
+            alignment_radius: None,
+            separation_radius: None,
         }
     }
 }
@@ -118,6 +230,14 @@ impl Default for Builder {
 pub struct Universe {
     boids: Vec<Boid>,
     boid_factory: Box<dyn BoidFactory>,
+    density: f32,
+    noise_fraction: f32,
+    attraction_weighting: u32,
+    alignment_weighting: u32,
+    separation_weighting: u32,
+    attraction_radius: f32,
+    alignment_radius: f32,
+    separation_radius: f32,
 }
 
 #[wasm_bindgen]
@@ -158,7 +278,6 @@ impl Universe {
 mod tests {
     use crate::universe::BoidFactory;
     use crate::{boid::Boid, *};
-    use std::collections::VecDeque;
 
     struct TestBoidFactory {
         boids: Vec<Boid>,
@@ -167,8 +286,9 @@ mod tests {
     impl BoidFactory for TestBoidFactory {
         fn create_n(&mut self, grid: &dyn grid::Grid<Boid>, number_of_boids: u32) -> Vec<Boid> {
             let mut result = Vec::new();
-            (0..number_of_boids)
-                .map(|_| result.push(self.boids.pop().expect("No more test boids in self.boids")));
+            (0..number_of_boids).for_each(|_| {
+                result.push(self.boids.pop().expect("No more test boids in self.boids"))
+            });
             result
         }
     }
@@ -256,5 +376,28 @@ mod tests {
             seen_velocities.push(boid.velocity);
             seen_accelerations.push(boid.acceleration);
         }
+    }
+
+    #[test]
+    fn attraction_rule() {
+        let b1 = Boid {
+            position: (1.0, 1.0),
+            velocity: (0.0, 0.0),
+            acceleration: (0.0, 0.0),
+        };
+        let b2 = Boid {
+            position: (2.0, 1.0),
+            velocity: (0.0, 0.0),
+            acceleration: (0.0, 0.0),
+        };
+        let universe = universe::Builder::from_preset(universe::Preset::Basic)
+            .boid_factory(Box::new(TestBoidFactory {
+                boids: vec![b1, b2],
+            }))
+            .number_of_boids(2)
+            .attraction_weighting(1)
+            .alignment_weighting(0)
+            .separation_weighting(0)
+            .build();
     }
 }
